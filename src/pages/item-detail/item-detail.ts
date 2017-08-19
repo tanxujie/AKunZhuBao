89b95b06f588c7390c4395d957f128abed24eb21
tanxujie <thunderhand@163.com>
2017-08-19 22:39:18 +0800

    share image in wechat timeline and session is ok

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -16,6 +16,32 @@ export class ItemDetailPage {
     this.item = navParams.get('item');// || items.defaultItem;
   }
 
+  shareWxSession() {
+    /*Wechat.isInstalled(function (installed) {
+      alert("Wechat installed: " + (installed ? "Yes" : "No"));
+    }, function (reason) {
+      alert("Failed: " + reason);
+    });*/
+
+    Wechat.share({
+      message: {
+        title: this.item.name,
+        description: this.item.description || '',
+        //thumb: this.item.imageName,
+        media: {
+          type: Wechat.Type.IMAGE,
+          image: this.item.imageName
+        }
+      },
+      //text: "This is just a plain string",
+      scene: Wechat.Scene.SESSION   // share to Session
+    }, function () {
+      alert("Success");
+    }, function (reason) {
+      alert("Share " + this.item.imageName + " Failed: " + reason);
+    });
+  }
+
   shareWxTimeLine() {
     /*Wechat.isInstalled(function (installed) {
       alert("Wechat installed: " + (installed ? "Yes" : "No"));
@@ -34,7 +60,7 @@ export class ItemDetailPage {
         }
       },
       //text: "This is just a plain string",
-      scene: Wechat.Scene.SESSION   // share to Timeline
+      scene: Wechat.Scene.TIMELINE   // share to Timeline
     }, function () {
       alert("Success");
     }, function (reason) {
