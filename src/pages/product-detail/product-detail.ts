21013576479c19ca34bee0f1f2d933b68f3289a0
tanxujie <thunderhand@163.com>
2017-08-26 00:45:26 +0800

    fixed wechat share image bug

--- src/pages/product-detail/product-detail.ts
+++ src/pages/product-detail/product-detail.ts
@@ -1,6 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Product } from '../../models/product';
+
+declare let Wechat;
+
 /**
  * Generated class for the ProductDetailPage page.
  *
@@ -27,10 +30,48 @@ export class ProductDetailPage {
   }
 
   shareWXTimeLine() {
-    alert("TimeLine");
+    Wechat.share({
+      message: {
+        title: this.currentProduct.code + ":" + this.currentProduct.name,
+        description: (this.currentProduct.description || ''),
+        //thumb: "www/img/thumbnail.png",
+        //mediaTagName: "TEST-TAG-001",
+        //messageExt: "这是第三方带的测试字段",
+        //messageAction: "<action>dotalist</action>",
+        media: {
+          type: Wechat.Type.IMAGE,
+          image: this.currentProduct.imageSrc
+        }
+      },
+      //text: "Just a test for session",
+      scene: Wechat.Scene.TIMELINE   // share to Timeline
+    }, function () {
+      alert("分享朋友圈成功");
+    }, function (reason) {
+      alert("分享朋友圈失败");
+    });
   }
 
   shareWXSession() {
-    alert("Session");
+    Wechat.share({
+      //text: "Just a test for session",
+      message: {
+        title: this.currentProduct.code + ":" + this.currentProduct.name,
+        description: (this.currentProduct.description|| ''),
+        //thumb: "www/img/thumbnail.png",
+        //mediaTagName: "TEST-TAG-001",
+        //messageExt: "这是第三方带的测试字段",
+        //messageAction: "<action>dotalist</action>",
+        media: {
+          type: Wechat.Type.IMAGE,
+          image: this.currentProduct.imageSrc
+        }
+      },
+      scene: Wechat.Scene.SESSION   // share to Timeline
+    }, function () {
+      alert("发送朋友成功");
+    }, function (reason) {
+      alert("发送朋友失败");
+    });
   }
 }
